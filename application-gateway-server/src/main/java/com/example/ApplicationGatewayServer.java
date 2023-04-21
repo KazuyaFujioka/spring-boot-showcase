9bc344270d0bb02d94ce54ae52ca3cfa915f1eba
kazuya.fujioka <kazuya.fujioka@gmail.com>
2023-04-21 19:52:14 +0900

    spring cloud gatewayでgRPCも動かせるように対応

---
+++ application-gateway-server/src/main/java/com/example/ApplicationGatewayServer.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationGatewayServer {
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationGatewayServer.class, args);
+  }
+}
