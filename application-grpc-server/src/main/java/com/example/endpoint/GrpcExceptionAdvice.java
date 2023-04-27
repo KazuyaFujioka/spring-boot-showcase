7f1ee86e0ff068ac358a9b4a8c9d25b06bb9d177
kazuya.fujioka <kazuya.fujioka@gmail.com>
2023-04-27 13:09:54 +0900

    GrpcExceptionAdviceにGRpcExceptionScope scopeがないと動作しないので元に戻す

--- application-grpc-server/src/main/java/com/example/endpoint/GrpcExceptionAdvice.java
+++ application-grpc-server/src/main/java/com/example/endpoint/GrpcExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.example.endpoint;
 import com.example.domain.policy.ResourceNotFoundException;
 import io.grpc.Status;
 import org.lognet.springboot.grpc.recovery.GRpcExceptionHandler;
+import org.lognet.springboot.grpc.recovery.GRpcExceptionScope;
 import org.lognet.springboot.grpc.recovery.GRpcServiceAdvice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,13 +14,13 @@ class GrpcExceptionAdvice {
   Logger LOG = LoggerFactory.getLogger(GrpcExceptionAdvice.class);
 
   @GRpcExceptionHandler
-  public Status notFound(ResourceNotFoundException exception) {
+  public Status notFound(ResourceNotFoundException exception, GRpcExceptionScope scope) {
     LOG.warn(exception.getMessage());
     return Status.NOT_FOUND.withDescription(exception.getMessage()).withCause(exception);
   }
 
   @GRpcExceptionHandler
-  public Status internalServerError(Exception exception) {
+  public Status internalServerError(Exception exception, GRpcExceptionScope scope) {
     LOG.error(exception.getMessage());
     return Status.INTERNAL.withDescription(exception.getMessage()).withCause(exception);
   }
