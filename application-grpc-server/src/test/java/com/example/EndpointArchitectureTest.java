0abcd7ddf69a6639515b2071ae882af827a17a59
kazuya.fujioka <kazuya.fujioka@gmail.com>
2023-04-28 14:09:06 +0900

    gRPCアプリケーションサーバーにArchUnitを追加

---
+++ application-grpc-server/src/test/java/com/example/EndpointArchitectureTest.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.lognet.springboot.grpc.GRpcService;
+
+/** エンドポイントが定義通りに実装が行われているかを確認するテスト */
+@AnalyzeClasses
+class EndpointArchitectureTest {
+
+  @ArchTest
+  static final ArchRule プレゼンテーションレイヤーのgRPCサービスクラスはパッケージプライベートである =
+      classes()
+          .that()
+          .haveSimpleNameEndingWith("ServiceImpl")
+          .and()
+          .resideInAPackage(LayeredArchitectureTest.endpointPackage)
+          .should()
+          .bePackagePrivate();
+
+  @ArchTest
+  static final ArchRule プレゼンテーションレイヤーのgRPCサービスクラスはGRpcServiceアノテーションが付与されている =
+      classes()
+          .that()
+          .haveSimpleNameEndingWith("ServiceImpl")
+          .and()
+          .resideInAPackage(LayeredArchitectureTest.endpointPackage)
+          .should()
+          .beAnnotatedWith(GRpcService.class);
+}
