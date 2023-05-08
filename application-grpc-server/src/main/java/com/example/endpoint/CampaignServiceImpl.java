e81e3a9f2a360c510077db1549384790d1a78f73
kazuya.fujioka <kazuya.fujioka@gmail.com>
2023-05-08 18:18:45 +0900

    protoファイルをinfrastructureからendpointへパッケージ移動

--- application-grpc-server/src/main/java/com/example/endpoint/CampaignServiceImpl.java
+++ application-grpc-server/src/main/java/com/example/endpoint/CampaignServiceImpl.java
@@ -3,7 +3,8 @@ package com.example.endpoint;
 import com.example.application.service.CampaignService;
 import com.example.domain.model.Campaign;
 import com.example.domain.model.Campaigns;
-import com.example.infrastructure.grpc.protobuf.service.CampaignServiceGrpc;
+import com.example.endpoint.grpc.protobuf.service.CampaignServiceGrpc;
+import com.example.endpoint.grpc.protobuf.type.Number;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 import org.slf4j.Logger;
@@ -22,13 +23,13 @@ class CampaignServiceImpl extends CampaignServiceGrpc.CampaignServiceImplBase {
   @Override
   public void findOngoingCampaign(
       com.google.protobuf.Empty request,
-      StreamObserver<com.example.infrastructure.grpc.protobuf.type.Campaigns> responseObserver) {
+      StreamObserver<com.example.endpoint.grpc.protobuf.type.Campaigns> responseObserver) {
     LOG.debug("find ongoing campaign");
 
     Campaigns campaigns = campaignService.findOngoingCampaigns();
 
-    com.example.infrastructure.grpc.protobuf.type.Campaigns.Builder gRPCCampaignsBuilder =
-        com.example.infrastructure.grpc.protobuf.type.Campaigns.newBuilder();
+    com.example.endpoint.grpc.protobuf.type.Campaigns.Builder gRPCCampaignsBuilder =
+        com.example.endpoint.grpc.protobuf.type.Campaigns.newBuilder();
     modelToGrpcConverter.convert(campaigns, gRPCCampaignsBuilder);
 
     responseObserver.onNext(gRPCCampaignsBuilder.build());
@@ -37,16 +38,16 @@ class CampaignServiceImpl extends CampaignServiceGrpc.CampaignServiceImplBase {
 
   @Override
   public void findCampaign(
-      com.example.infrastructure.grpc.protobuf.type.Number request,
-      StreamObserver<com.example.infrastructure.grpc.protobuf.type.Campaign> responseObserver) {
+      Number request,
+      StreamObserver<com.example.endpoint.grpc.protobuf.type.Campaign> responseObserver) {
     LOG.debug("find campaign");
 
     com.example.domain.model.Number number =
         grpcToModelConverter.convert(request, com.example.domain.model.Number.class);
     Campaign campaign = campaignService.findCampaign(number);
 
-    com.example.infrastructure.grpc.protobuf.type.Campaign.Builder gRPCCampaignBuilder =
-        com.example.infrastructure.grpc.protobuf.type.Campaign.newBuilder();
+    com.example.endpoint.grpc.protobuf.type.Campaign.Builder gRPCCampaignBuilder =
+        com.example.endpoint.grpc.protobuf.type.Campaign.newBuilder();
     modelToGrpcConverter.convert(campaign, gRPCCampaignBuilder);
 
     responseObserver.onNext(gRPCCampaignBuilder.build());
