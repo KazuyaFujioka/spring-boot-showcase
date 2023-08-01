5ea9923d1bc099467e90c20d0222e8b43af4af91
kazuya.fujioka <kazuya.fujioka@gmail.com>
2023-08-01 19:16:56 +0900

    protoファイルを共通で使えるようにモジュール化

--- application-server/src/main/java/com/example/_configuration/WebConfiguration.java
+++ application-server/src/main/java/com/example/_configuration/WebConfiguration.java
@@ -6,7 +6,13 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.YearMonthSerializer;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,8 +35,17 @@ class WebConfiguration implements WebMvcConfigurer {
     objectMapper.setVisibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE);
     objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-    objectMapper.registerModule(new JavaTimeModule());
+    JavaTimeModule module = new JavaTimeModule();
+    module.addSerializer(new LocalDateSerializer(DateTimeFormatter.ISO_DATE));
+    module.addSerializer(
+        new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+    module.addSerializer(new YearMonthSerializer(DateTimeFormatter.ofPattern("yyyy-MM")));
 
+    module.addDeserializer(
+        LocalDateTime.class,
+        new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+    objectMapper.registerModule(module);
     return objectMapper;
   }
 
